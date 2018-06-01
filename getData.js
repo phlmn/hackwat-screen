77a37d3550773bf75cb0e6842191acd0727807aa
Philipp Mandler <info@philipp-mandler.com>
2018-06-01 11:42:57 +0200

    initial commit

---
+++ getData.js
@@ -0,0 +1,110 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+function getData() {
+    return fetch('https://flipdot.org/wiki/HackWat?action=raw').then((res) => {
+        return res.text();
+    });
+}
+
+function getLine(lines, text) {
+    let lineNo = null;
+
+    lines.forEach((line, index) => {
+        const start = line.indexOf(text);
+        if (start !== -1) {
+            lineNo = index;
+        }
+    });
+
+    return lineNo;
+}
+
+function getTableRow(line) {
+    line = line.trim();
+    if (line.startsWith('||') && line.endsWith('||')) {
+        // remove start and end
+        line = line.substring(2, line.length - 2);
+    } else {
+        return null;
+    }
+
+    return line.split('||').map(header => header.trim());
+}
+
+function parseTable(lines, startLineNo) {
+    let currentLineNo = startLineNo;
+    const table = [];
+
+    while (true) {
+        const line = lines[currentLineNo];
+        const row = getTableRow(line);
+
+        if (!row) {
+            break;
+        }
+
+        table.push(row);
+        currentLineNo += 1;
+    }
+
+    return table;
+}
+
+function getRowNo(table, text) {
+    let rowNo = null;
+
+    table.forEach((row, index) => {
+        if (row.some(col => col.indexOf(text) !== -1)) {
+            rowNo = index;
+        }
+    });
+
+    return rowNo;
+}
+
+function mapRowToTalk(row) {
+    return {
+        start: row[0],
+        end: row[1],
+        speaker: row[2],
+        title: row[3],
+        room: row[4],
+        recorded: row[5],
+    };
+}
+
+app.get('/', (req, res) => {
+    getData().then((data) => {
+        const lines = data.split('\n');
+
+        const titleLineNo = getLine(lines, `2018-06-01 Freitag`);
+
+        const table = parseTable(lines, titleLineNo);
+
+        const day1Start = getRowNo(table, '2018-06-01');
+        const day2Start = getRowNo(table, '2018-06-02');
+        const day3Start = getRowNo(table, '2018-06-03');
+
+        const day1Table = table.slice(2, day2Start);
+        const day2Table = table.slice(day2Start + 2, day3Start);
+        const day3Table = table.slice(day3Start + 2);
+
+        res.send({
+            talks: {
+                '2018-06-01': day1Table.map(mapRowToTalk),
+                '2018-06-02': day2Table.map(mapRowToTalk),
+                '2018-06-03': day3Table.map(mapRowToTalk),
+            }
+        });
+    });
+});
+
+app.listen(4444, () => {
+    console.log('Listening…');
+});
+
---
+++ index.jsx
@@ -0,0 +1,188 @@
+import classNames from 'classnames';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import format from 'date-fns/format';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lines: [],
+        };
+    }
+
+    componentDidMount() {
+        this.printTodaysTalks();
+    }
+
+    printTodaysTalks = () => {
+        fetch('http://phl.mn:4444/').then(res => res.json()).then((data) => {
+            const today = format(new Date(), 'YYYY-MM-DD');
+
+            this.writeLine('');
+            this.writeLine('HEUTIGE TALKS');
+            this.writeSeparator();
+            data.talks[today].forEach(({ title, speaker, start, end, room }) => {
+                this.writeLine(`'${title}' - ${speaker}`);
+                this.writeLine(`${start} bis ${end} in Raum ${room}`);
+                this.writeLine('');
+            });
+        });
+    }
+
+    writingFinished = () => {
+        this.printTodaysTalks();
+    }
+
+    render() {
+        return (
+            <div onClick={() => { this.writeLine('adasdasd ad asda sd asd as das d ') }}>
+                <AwesomeConsole text={this.state.lines} finished={this.writingFinished}/>
+                <div className="crt"/>
+                <div className="vignette"/>
+            </div>
+        );
+    }
+
+    writeLine(text) {
+        this.setState(oldState => ({
+            lines: [...oldState.lines, text],
+        }));
+    }
+
+    writeSeparator() {
+        this.writeLine('---------------------------------------------------');
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+class AwesomeConsole extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lines: [''],
+            currentLine: 0,
+            idle: true,
+        };
+    }
+
+    componentDidMount() {
+        // start typing
+        setTimeout(() => this.type(), 100);
+    }
+
+    type(isRepeating = false, fixesTypo = false) {
+        const { currentLine, lines } = this.state;
+
+        let delay = 100 * ((Math.random() - 0.5) * 1.2 + 1.0);
+        let nextRepeating = false;
+
+        const wantedText = this.props.text[currentLine];
+        const currentText = lines[currentLine];
+
+        if (wantedText === undefined) {
+            this.setState({
+                idle: true,
+            });
+        } else if(wantedText !== currentText) {
+            this.finishedNotified = true;
+
+            this.setState({
+                idle: false,
+            });
+
+            if (wantedText.substring(0, currentText.length) !== currentText) {
+                // has typo
+                if (getRandomInt(10) === 0) {
+                    fixesTypo = true;
+                }
+            } else {
+                fixesTypo = false;
+            }
+
+            let newLine;
+
+            if (fixesTypo) {
+                newLine = currentText.substr(0, currentText.length - 1);
+            } else {
+                let newChar = wantedText[currentText.length];
+
+                if (newChar == undefined) {
+                    // end of line and has type -> go fix it
+                    fixesTypo = true;
+                    newLine = currentText.substr(0, currentText.length - 1);
+                } else {
+                    if (!isRepeating && getRandomInt(50) === 0) {
+                        // do typo
+                        newChar = String.fromCharCode(newChar.charCodeAt(0) + 1);
+                    }
+
+                    if (wantedText[currentText.length + 1] === newChar && wantedText[currentText.length + 2] === newChar) {
+                        if (currentText[currentText.length - 1] === newChar) {
+                            delay = 30;
+                            nextRepeating = true;
+                        }
+                        else {
+                            delay = 500;
+                            nextRepeating = true;
+                        }
+                    }
+
+                    newLine = currentText + newChar;
+                }
+            }
+
+
+            this.setState({
+                lines: [...lines.slice(0, lines.length - 1), newLine],
+            });
+
+        } else if (this.props.text.length - 1 > currentLine) {
+            this.finishedNotified = false;
+
+            // new line
+            this.setState({
+                lines: [...lines, ''],
+                currentLine: currentLine + 1,
+            });
+            delay = delay * 2;
+        } else {
+            // idle
+            this.setState({
+                idle: true,
+            });
+
+            if (!this.finishedNotified && this.props.finished) {
+                this.finishedNotified = true;
+                this.props.finished();
+            }
+        }
+
+        setTimeout(() => this.type(nextRepeating, fixesTypo), delay);
+    }
+
+    render() {
+        return (
+            <div className="console-text">
+                {this.state.lines.map((line, index) => (
+                    <span key={index}>{line}{(index !== this.state.lines.length - 1) && <br />}</span>
+                ))}
+                <Cursor idle={this.state.idle}/>
+            </div>
+        );
+    }
+}
+
+function Cursor({ idle }) {
+    return (
+        <span className={classNames('cursor', { idle })}>_</span>
+    );
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
